5709e7b20bc0b2e5573dab51dd0013f36d0d2fba
aSykoutris <134492329+aSykoutris@users.noreply.github.com>
2024-01-18 10:44:11 +0200

    Added text

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -15,24 +15,33 @@ import { CorkedBottle } from "@/components/3D/CorkedBottle";
 import Particles from "@/components/3D/Particles";
 import { Bike } from "@/components/3D/Bike";
 import { GridLock } from "@/components/3D/GridLock";
+import Typewriter from "@/components/CodeBlock/Typewriter";
 
 type Props = {};
 
 export default function Hero({}: Props) {
   return (
-    <section className="h-screen w-full">
+    <section className="relative h-screen w-full">
       <Canvas>
         <Environment preset="night" />
         <ScrollControls pages={6} damping={0.25}>
-          <Scroll></Scroll>
+          <Scroll>
+            <mesh rotation={[1, 2, 1]}>
+              <boxGeometry />
+              <meshBasicMaterial color={"orange"} />
+            </mesh>
+          </Scroll>
           <Scroll html style={{ width: "100%" }}>
-            <h1 className="absolute left-1/2 top-[45vh] -translate-x-1/2 -translate-y-1/2 transform text-9xl text-gray-300">
+            <h1 className="absolute left-1/2 top-[45vh]   -translate-x-1/2 -translate-y-1/2 transform text-9xl text-gray-300">
               THIS IS CENTER.
             </h1>
             <div className="absolute top-[132vh] text-gray-300">
               <h2 className="text-6xl font-bold text-gray-300">
                 THIS IS LEFT.
               </h2>
+              <div>
+                <Typewriter />
+              </div>
             </div>
 
             <div className="absolute top-[230vh] w-full">
